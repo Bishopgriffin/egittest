f1c8581166cb533b9ecc01f7198fffea4f3fe44e
Dewet Blaauw <dewet@amaras.co.za>
2019-02-18 10:37:54 +0200

    Added the next line of code

--- EgittyTest/src/za/co/amaras/testing/start/Start.java
+++ EgittyTest/src/za/co/amaras/testing/start/Start.java
@@ -8,6 +8,9 @@ public class Start {
 		
 		//Adding the new line of code here for testing
 		System.out.println ("New line of code");
+		
+		// Adding the senconf line of code
+		System.out.println ("Next Line of Code");
 
 	}
 
